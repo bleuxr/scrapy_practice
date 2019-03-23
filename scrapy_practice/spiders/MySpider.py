f628f4b0b803bedf830f4da04c818eee8bfce4ed
bleuxr <38353817+bleuxr@users.noreply.github.com>
2019-03-23 22:25:58 +0800

    start_requests()

---
+++ scrapy_practice/spiders/MySpider.py
@@ -0,0 +1,13 @@
+import scrapy
+
+
+class MySpider(scrapy.Spider):
+    name = 'myspider'
+
+    def start_requests(self):
+        return [scrapy.FormRequest("http://www.example.com/login",formdata={'user': 'john', 'pass': 'secret'},callback=self.logged_in)]
+
+    def logged_in(self, response):
+        # here you would extract links to follow and return Requests for
+        # each of them, with another callback
+        pass
