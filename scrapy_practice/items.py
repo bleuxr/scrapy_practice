351f783d53aa3f48186a4f29c258efac9d1be3ff
bleuxr <38353817+bleuxr@users.noreply.github.com>
2019-03-23 23:20:40 +0800

    add TestItem

--- scrapy_practice/items.py
+++ scrapy_practice/items.py
@@ -12,3 +12,8 @@ class ScrapyPracticeItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class TestItem(scrapy.Item):
+    id = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
