d9d2229557eda33a47a9a3558b237efd7dd7f7a5
bleuxr <38353817+bleuxr@users.noreply.github.com>
2019-03-24 17:12:27 +0800

    CSVFeedSpider

---
+++ scrapy_practice/spiders/CSVFeedSpider.py
@@ -0,0 +1,19 @@
+from scrapy.spiders import CSVFeedSpider
+from scrapy_practice.items import TestItem
+
+class MySpider(CSVFeedSpider):
+    name = 'example.com_CSVFeedSpider'
+    allowed_domains = ['example.com']
+    start_urls = ['http://www.example.com/feed.csv']
+    delimiter = ';'
+    quotechar = "'"
+    headers = ['id', 'name', 'description']
+
+    def parse_row(self, response, row):
+        self.logger.info('Hi, this is a row!: %r', row)
+
+        item = TestItem()
+        item['id'] = row['id']
+        item['name'] = row['name']
+        item['description'] = row['description']
+        return item
